fbd12d662bb0e5ebb9d41fd4046713c4a5b0794b
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-07 13:38:02 +0100

    finished

---
+++ 1_2/main.js
@@ -0,0 +1,10 @@
+const person = {
+    name: "Sven",
+    alter:29,
+    sagNameAlter: () => {
+        alert(person.name + " " + person.alter)
+        console.log(person.name,person.alter)
+    }
+}
+person.sagNameAlter()
+
